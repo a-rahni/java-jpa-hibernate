d26231bada124550dbb779320406258312b154e9
a rahni <rahniahmed@gmail.com>
2022-08-17 11:06:12 +0200

    config project with jpa hibernate

--- src/main/java/fr/m2i/javajpahibernate/JavaJpaHibernate.java
+++ src/main/java/fr/m2i/javajpahibernate/JavaJpaHibernate.java
@@ -1,9 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package fr.m2i.javajpahibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -12,6 +12,9 @@ package fr.m2i.javajpahibernate;
 public class JavaJpaHibernate {
 
     public static void main(String[] args) {
+      
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("site_commerce_pu");
+        EntityManager entityManager = emf.createEntityManager();
         System.out.println("Hello World!");
     }
 }
---
+++ src/main/java/fr/m2i/javajpahibernate/helper/SessionHelper.java
@@ -0,0 +1,16 @@
+
+package fr.m2i.javajpahibernate.helper;
+
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author rahni
+ */
+public class SessionHelper {
+    
+    //private static EntityManager entityManager;
+    /*to do*/
+ 
+    
+}
